0c0b90dab8c59402bd50b1dbf1fd2e75dcc684b3
TheFenrisLycaon <thefenrislycaon@gmail.com>
2021-08-06 17:26:47 +0530

    mikro-ORM setup

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,20 @@
-console.log("Hello World!!!");
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const Post_1 = require("./entities/Post");
+const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const main = async () => {
+    const orm = await core_1.MikroORM.init(mikro_orm_config_1.default);
+    const post = orm.em.create(Post_1.Post, { title: "Hello World!" });
+    await orm.em.persistAndFlush(post);
+    console.log("=======SQL=========");
+    const posts = await orm.em.find(Post_1.Post, {});
+    console.log(posts);
+};
+main().catch((err) => {
+    console.error(err);
+});
 //# sourceMappingURL=index.js.map
