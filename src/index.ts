f5fa0c271e3b1d09988851f65f6d6231c41677a4
TheFenrisLycaon <thefenrislycaon@gmail.com>
2021-09-12 08:49:27 +0530

    Setup Mikro-ORM

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Welcome Fenris");
+import { MikroORM } from "@mikro-orm/core";
+import microConfig from "./mikro-orm.config";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+};
+
+main().catch((err) => {
+  console.error(err);
+});
