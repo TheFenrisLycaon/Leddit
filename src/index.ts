30ca9cd4c2bd69d879d59d7d5041d85e077e3b19
TheFenrisLycaon <thefenrislycaon@gmail.com>
2021-09-17 12:19:19 +0530

    Register

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { MikroORM } from "@mikro-orm/core";
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
+import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import microConfig from "./mikro-orm.config";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -13,12 +16,12 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
-  await apolloServer.start()
-  
+
   apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
